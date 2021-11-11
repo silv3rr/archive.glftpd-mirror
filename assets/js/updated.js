2172ec51e10b569290087d383f515edc140f4ca7
silver <silv3rr@users.noreply.github.com>
2021-11-11 20:42:31 +0100

    gh-pages

---
+++ assets/js/updated.js
@@ -0,0 +1,20 @@
+function lastUpdated() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("HEAD", "README.md", true);
+  xhr.onload = function (e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+	u = new Date(xhr.getResponseHeader("Last-Modified"));
+	var y = u.getFullYear(); var m = u.getMonth()+1; var d = u.getDate(); var H = u.getHours(); var M = u.getMinutes();
+	document.getElementById('lastUpdated').innerHTML = "updated " + y + '-' + (m<10 ? '0' : '') + m + '-' + (d<10 ? '0' : '') + d + ' ' + (H<10 ? '0' : '') + H + ':' + (M<10 ? '0' : '') + M;
+      } else {
+	document.getElementById('lastUpdated').innerHTML = "updated n/a";
+      }
+    }
+  };
+  xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+  };
+  xhr.send(null);
+};
+window.onload = lastUpdated;
