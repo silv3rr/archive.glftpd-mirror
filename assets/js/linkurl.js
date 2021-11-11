42ca27ac50f2f0f036eefbd0684246f3c6276b07
silver <silv3rr@users.noreply.github.com>
2021-11-11 20:54:52 +0100

    gh-pages

--- assets/js/linkurl.js
+++
@@ -1,4 +0,0 @@
-function linkURL(text) {
-  var re = RegExp("(\\b((https?|ftp|file):\/\/|www)[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]*)", "gi");
-  document.documentElement.innerHTML = text.replace(re,"<a href='$1'>$1<\/a>");
-}
